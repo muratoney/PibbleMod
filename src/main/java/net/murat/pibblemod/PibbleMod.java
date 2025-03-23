4d635794a69cef3906dbfcb6a51640df19130728
muratoney <murato366@gmail.com>
2025-03-23 14:16:36 -0500

    Initial commit

---
+++ src/main/java/net/murat/pibblemod/PibbleMod.java
@@ -0,0 +1,16 @@
+package net.murat.pibblemod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PibbleMod implements ModInitializer {
+	public static final String MOD_ID = "pibble-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/murat/pibblemod/PibbleModClient.java
@@ -0,0 +1,10 @@
+package net.murat.pibblemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PibbleModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
